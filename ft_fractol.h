15dd2c600e51120bb53ddd2a3110240566ea3150
halit yakici <halit.yakici.hy@gmail.com>
2025-07-07 19:34:27 +0300

    ARGUMENTS OK

---
+++ ft_fractol.h
@@ -0,0 +1,14 @@
+
+
+#ifndef FT_FRACTOL_H
+#define FT_FRACTOL_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int	ft_strcmp(const char *s1, const char *s2);
+
+
+
+
+#endif
---
+++ main.c
@@ -0,0 +1,19 @@
+#include "minilibx-linux/mlx.h"
+#include "ft_fractol.h"
+
+
+
+int	main(int argc, char **argv)
+{
+	if ((argc == 2 && !ft_strcmp(argv[1], "Mandelbrot")) || (argc == 4 && !ft_strcmp(argv[1], "Julia")))
+	{
+		printf("DORU\n");
+	}
+	else
+	{
+		// HATA MESAJİ YAZ BURA 
+		// GEÇERLI SETLERI LISTELE
+	}
+	
+	
+}
