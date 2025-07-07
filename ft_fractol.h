4dcb6bf3d688dd07f173d933796d5fdea02e6eb8
halit yakici <halit.yakici.hy@gmail.com>
2025-07-07 22:38:03 +0300

    ilk aşamada argumanlar doğru aliyor pencere normal açılıyor kapanırken leak oluşmuyor

--- ft_fractol.h
+++ ft_fractol.h
@@ -4,7 +4,34 @@
 #define FT_FRACTOL_H
 
 #include <stdlib.h>
+#include "minilibx-linux/mlx.h"
 #include <stdio.h>
+#include <unistd.h>
+#include <math.h>
+
+#define HEIGHT 800
+#define WIDTH 800
+
+
+typedef struct s_img
+{
+	void *img_ptr;
+	char *pixel_ptr;
+	int	bitperpixel;
+	int endian;
+	int	size_line;
+}			t_img;
+
+
+
+typedef struct s_fractol
+{
+	void *f_connection;
+	void *f_window;
+	t_img	img;
+}			t_fractol;
+
+
 
 int	ft_strcmp(const char *s1, const char *s2);
 
--- main.c
+++ main.c
@@ -1,19 +1,49 @@
-#include "minilibx-linux/mlx.h"
 #include "ft_fractol.h"
 
+// cc main.c utils.c -Iminilibx-linux -Lminilibx-linux -lmlx -lXext -lX11 -lm -lz
+void	fractal_initializer(t_fractol *fract, char *name)
+{
+	fract->f_connection = mlx_init();
+	fract->f_window = mlx_new_window(fract->f_connection, WIDTH, HEIGHT, name);
+	if (!fract->f_window)
+	{
+		mlx_destroy_display(fract->f_connection);
+		free(fract->f_connection);
+		return ;
+	}
+	fract->img.img_ptr = mlx_new_image(fract->f_connection,WIDTH,HEIGHT);
+	if (!fract->img.img_ptr)
+	{
+		mlx_destroy_window(fract->f_connection, fract->f_window);
+		mlx_destroy_display(fract->f_connection);
+		free(fract->f_connection);
+		return ;
+    }
+	fract->img.pixel_ptr = mlx_get_data_addr(fract->img.img_ptr,&fract->img.bitperpixel,&fract->img.size_line, &fract->img.endian);
+}
 
+int	cls_win(t_fractol *fract)
+{
+	mlx_destroy_image(fract->f_connection,fract->img.img_ptr);
+	mlx_destroy_window(fract->f_connection, fract->f_window);
+	mlx_destroy_display(fract->f_connection);
+	free(fract->f_connection);
+	exit(0);
+}
 
 int	main(int argc, char **argv)
 {
+	t_fractol frctl;
 	if ((argc == 2 && !ft_strcmp(argv[1], "Mandelbrot")) || (argc == 4 && !ft_strcmp(argv[1], "Julia")))
 	{
-		printf("DORU\n");
+		fractal_initializer(&frctl, argv[1]);
+		mlx_hook(frctl.f_window, 17, 0, cls_win, &frctl);
+		mlx_loop(frctl.f_connection);		
 	}
 	else
 	{
 		// HATA MESAJİ YAZ BURA 
 		// GEÇERLI SETLERI LISTELE
+		exit(1);
 	}
-	
-	
 }
